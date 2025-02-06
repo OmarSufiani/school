056293151abe0ce854429fe97fb490010fd635d7
OmarSufiani <hommiedelaco@gmail.com>
2025-02-06 09:36:44 +0300

    register

--- registerr.php
+++ registerr.php
@@ -2,6 +2,17 @@
 // Start the session if you plan to use session data (optional)
 session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
  
    
  <!-- Email input -->
